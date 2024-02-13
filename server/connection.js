bbbb56e3037485ef37281cddfb4e0d14ccabb3d3
AAADHIRAAA <padmapriyaindia2004@gmail.com>
2024-02-13 11:47:49 +0530

    connection string changed

--- server/connection.js
+++ server/connection.js
@@ -2,12 +2,11 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './.env' });
 
+const uri='mongodb://localhost:27017/search'
 const connectDB = async () => {
   try {
-    const atlasConnectionString = process.env.ATLAS_STRING;
-    await mongoose.connect(atlasConnectionString, {
-
-    });
+  
+    await mongoose.connect(uri);
     console.log('Connected to MongoDB Atlas');
   } catch (error) {
     console.error('MongoDB Connection Error:', error);
